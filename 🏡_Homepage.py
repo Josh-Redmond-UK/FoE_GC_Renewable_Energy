605b51f3110055474489ca6d4c45464da2b97005
arvdv <a.vandervoort@exeter.ac.uk>
2022-07-13 16:54:23 +0100

    merged

--- 🏡_Homepage.py
+++ 🏡_Homepage.py
@@ -7,10 +7,4 @@ import streamlit as st
 import ee
 st.set_page_config(page_title = "Homepage")
 
-<<<<<<< HEAD
 st.title("UK Renewable Energy Potential", anchor=None)
-=======
-st.title("UK Renewable Energy Potential", anchor=None)
-
-st.write(ee.Authenticate())
->>>>>>> 1ea28084f6da4c28d9efa00be19b63119b13ff0c
