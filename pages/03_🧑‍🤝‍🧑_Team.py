84db3ba2d710b134347a56342bd42fb82badb46c
jr725 <jr725@exeter.ac.uk>
2022-07-14 23:21:03 +0100

    randomised order of team page

--- pages/03_🧑‍🤝‍🧑_Team.py
+++ pages/03_🧑‍🤝‍🧑_Team.py
@@ -1,67 +1,74 @@
 import streamlit as st
-
+import random
 
 st.markdown('''
 ### Meet our team:''')
+def josh():
+    with st.container():
+        c1, c2 = st.columns(2)
+        with c1:
+            st.image('josh-redmond-photo.jpg')
+        with c2:
+            st.markdown('''Josh Redmond: *"I am a PhD student at Exeter University studying the use of satellite images as forensics in Human Rights Cases within a participatory design framework"*''')
 
-with st.container():
-    c1, c2 = st.columns(2)
-    with c1:
-        st.image('josh-redmond-photo.jpg')
-    with c2:
-        st.markdown('''Josh Redmond: *"I am a PhD student at Exeter University studying the use of satellite images as forensics in Human Rights Cases within a participatory design framework"*''')
+def arthur():
+    with st.container():
+        c1, c2 = st.columns(2)
+        with c1:
+            st.image('arthur_vandervoort.jpeg')
+        with c2:
+            st.markdown('''Arthur Vandervoort: *"I'm a PhD student at the University of Exeter's CDT in Environmental Intelligence, researching the impact of gender on urban mobility patterns."*' ''')
 
+def ellie():
+    with st.container():
+        c1, c2 = st.columns(2)
+        with c1:
+            st.image('Ellie_Fox.jpg')
+        with c2:
+            st.markdown('Ellie Fox: *"I am a PhD student in Environmental Intelligence at the University of Exeter, researching how melting glaciers impact downstream communities."*')
 
-with st.container():
-    c1, c2 = st.columns(2)
-    with c1:
-        st.image('arthur_vandervoort.jpeg')
-    with c2:
-        st.markdown('''Arthur Vandervoort: *"I'm a PhD student at the University of Exeter's CDT in Environmental Intelligence, researching the impact of gender on urban mobility patterns."*' ''')
+def alice():
+    with st.container():
+        c1, c2 = st.columns(2)
+        with c1:
+            st.image('alice_lake.jpg')
+        with c2:
+            st.markdown('Alice Lake: *"I am a Foundation Scientist at the Met Office, working in the Post-processing Applications team."*')
 
+def danielle():
+    with st.container():
+        c1, c2 = st.columns(2)
+        with c1:
+            st.image('Danielle Waters.jpg')
+        with c2:
+            st.markdown('Danielle Waters: *"I work as a freelance hydrographic surveyor, primarily working within the offshore wind renewable sector."*')
 
-with st.container():
-    c1, c2 = st.columns(2)
-    with c1:
-        st.image('Ellie_Fox.jpg')
-    with c2:
-        st.markdown('Ellie Fox: *"I am a PhD student in Environmental Intelligence at the University of Exeter, researching how melting glaciers impact downstream communities."*')
+def matthew():
+    with st.container():
+        c1, c2 = st.columns(2)
+        with c1:
+            st.image('Matthew Hayslep.jpg')
+        with c2:
+            st.markdown('Matthew Hayslep: *"I am a PhD student at the University of Exeter, researching leakage in water distribution systems using machine learning."*')
 
+def rodrigo():
+    with st.container():
+        c1, c2 = st.columns(2)
+        with c1:
+            st.image('Rodrigo_Arce.png')
+        with c2:
+            st.markdown('Rodrigo Arce: *"I work at Google in the Google Cloud Platform team, giving support to clients who use data analysis products."*')
+def lizzy():
+    with st.container():
+        c1, c2 = st.columns(2)
+        with c1:
+            pass
+        with c2:
+            st.markdown('''Elizabeth Galloway: ''')
 
-with st.container():
-    c1, c2 = st.columns(2)
-    with c1:
-        st.image('alice_lake.jpg')
-    with c2:
-        st.markdown('Alice Lake: *"I am a Foundation Scientist at the Met Office, working in the Post-processing Applications team."*')
+team = [josh, arthur, danielle, alice, matthew, rodrigo, lizzy]
 
+random.shuffle(team)
 
-with st.container():
-    c1, c2 = st.columns(2)
-    with c1:
-        st.image('Danielle Waters.jpg')
-    with c2:
-        st.markdown('Danielle Waters: *"I work as a freelance hydrographic surveyor, primarily working within the offshore wind renewable sector."*')
-
-
-with st.container():
-    c1, c2 = st.columns(2)
-    with c1:
-        st.image('Matthew Hayslep.jpg')
-    with c2:
-        st.markdown('Matthew Hayslep: *"I am a PhD student at the University of Exeter, researching leakage in water distribution systems using machine learning."*')
-
-
-with st.container():
-    c1, c2 = st.columns(2)
-    with c1:
-        st.image('Rodrigo_Arce.png')
-    with c2:
-        st.markdown('Rodrigo Arce: *"I work at Google in the Google Cloud Platform team, giving support to clients who use data analysis products."*')
-
-with st.container():
-    c1, c2 = st.columns(2)
-    with c1:
-        pass
-    with c2:
-        st.markdown('''Elizabeth Galloway: ''')
+for i in team:
+    i()
