91cdbf76e5d7bdf1ba14e93f09a87fb1b942f930
jr725 <jr725@exeter.ac.uk>
2022-07-14 14:17:15 +0100

    changed to folium static

--- pages/01_🌍_Map.py
+++ pages/01_🌍_Map.py
@@ -177,7 +177,7 @@ if go_button:
 
     m.addLayer(windpower_adj, {"min":minvis, "max":maxvis, "palette":['#140b34', '#84206b', '#e55c30', '#f6d746']})
     m.add_colorbar_branca(colors=['#140b34', '#84206b', '#e55c30', '#f6d746'], vmin=minvis, vmax=maxvis, layer_name="Potential Power")
-    st_folium(m, height=700)
+    folium_static(m, height=700)
     st.download_button("Download Map", "null", f"{area}-{mode}.txt")
 
 
