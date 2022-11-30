3d70060725edb93413113d64501d1bff538d7ab8
jr725 <jr725@exeter.ac.uk>
2022-11-30 15:00:21 +0000

    basic flask api to return potential vals

--- flaskApp.py
+++ flaskApp.py
@@ -2,28 +2,42 @@ from utils import *
 from flask import Flask
 from flask import jsonify
 app = Flask(__name__)
-
+ee.Initialize(opt_url='https://earthengine-highvolume.googleapis.com')
 @app.route('/<postcode>')
 def renewablePotential(postcode):  
-    ladGeometries = postcodeToGeom(postcode)
-
-    activeGeom = postcodeToGeom(postcode, ladGeometries) ## Works with LAD only for near you page
+    ladGeometries = ladsVector()
+        #LADs = 
+    activeGeom = ee.Feature(postcodeToGeom(postcode, ladGeometries)).geometry() ## Works with LAD only for near you page
 
     # Write utils default exclusions
     exclusionsDict = getExclusionsDict()
-    windExclusions = getExclusions("common") + getExclusions("wind")
-    solarExclusions = getExclusions("common") + getExclusions("solar")
-    
-    windExclusions = [exclusionsDict[x] for x in windExclusions]
-    solarExclusions = [exclusionsDict[x] for x in solarExclusions]
-    
+    windExclusions = getExclusions("wind") +  getExclusions("common")
+    solarExclusions = getExclusions("solar")+ getExclusions("common")
+
+    windExclusionImages = []
+    solarExclusionImages = []
+    for x in windExclusions:
+        try:
+            windExclusionImages.append(exclusionsDict[x])
+        except:
+            pass
+    for x in solarExclusions:
+        try:
+            solarExclusionImages.append(exclusionsDict[x])
+        except:
+            pass
+
+
+    #windExclusions = [exclusionsDict[x] for x in windExclusions]
+    #solarExclusions = [exclusionsDict[x] for x in solarExclusions]
+    windExclusionImages = compute_exclusions(windExclusionImages)
+    solarExclusionImages = compute_exclusions(solarExclusionImages)
     windPotential = windPowerRaster()
     solarPotential = solarPowerRaster()
+    localWind = windPotential.multiply(windExclusionImages).reduceRegion(ee.Reducer.sum(), activeGeom).getInfo()
+    localSolar = solarPotential.multiply(solarExclusionImages).reduceRegion(ee.Reducer.sum(), activeGeom).getInfo()
 
-    localWind = getPowerLocal(activeGeom, windExclusions, windPotential)
-    localSolar = getPowerLocal(activeGeom, solarExclusions, solarPotential)
     totalArea = usableAreaPerGeom(activeGeom, windPotential.gt(0).Or(solarPotential.gt(0)))
 
-    return jsonify(DevelopmentArea = totalArea, SolarPotential = localSolar, windPotential = localWind)
-
-    
+    testdict =  {"Development Area": totalArea, "Solar Potential": localSolar, "Wind Potential": localWind}#jsonify(DevelopmentArea = totalArea, SolarPotential = localSolar, windPotential = localWind)
+    return testdict
