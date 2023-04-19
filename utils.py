5cca98cc5f3c7790d0fb3e15329b7661cf4e766c
Josh-Redmond-UK <60511062+Josh-Redmond-UK@users.noreply.github.com>
2023-04-19 15:08:21 +0100

    Changed buffer added Scottish data

--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@
 import streamlit as st
 from streamlit_folium import folium_static
 import ee
-import geemap
+import geemap#.eefolium as geemap
 import csv
 import pandas as pd
 import logging
@@ -76,28 +76,31 @@ def getExclusionsDict():
     "Slope": ee.Terrain.slope(ee.Image("USGS/SRTMGL1_003")).lt(15),
     "Slope > 10": ee.Terrain.slope(ee.Image("USGS/SRTMGL1_003")).lt(10),
     "Transmission Lines":ee.FeatureCollection('projects/data-sunlight-311713/assets/transmission').reduceToImage(properties= ['FEATCODE'], reducer= ee.Reducer.first()).unmask().lt(1),
-    "Roads": ee.FeatureCollection('projects/data-sunlight-311713/assets/UK_Roads_Buffer_200m').reduceToImage(properties= ['FEATCODE'], reducer= ee.Reducer.first()).unmask().lt(1),
+    "Roads": ee.FeatureCollection('projects/data-sunlight-311713/assets/UK_Roads_Buffer_200m').map(lambda x: x.buffer(50)).reduceToImage(properties= ['FEATCODE'], reducer= ee.Reducer.first()).unmask().lt(1),
     "Peatland": ee.FeatureCollection('projects/data-sunlight-311713/assets/merged_peatlands').reduceToImage(properties = ['Shape__Are'], reducer= ee.Reducer.first()).unmask().lt(1),
     "Woodlands": ee.FeatureCollection('projects/data-sunlight-311713/assets/woodlands').reduceToImage(properties= ['FEATCODE'], reducer = ee.Reducer.first()).unmask().lt(1),
-    "Cycle Paths": ee.FeatureCollection('projects/data-sunlight-311713/assets/cyclenet').reduceToImage(properties= ['FID'], reducer= ee.Reducer.first()).unmask().lt(1),
+    "Cycle Paths Wind": ee.FeatureCollection('projects/data-sunlight-311713/assets/cyclenet').map(lambda x: x.buffer(100)).reduceToImage(properties= ['FID'], reducer= ee.Reducer.first()).unmask().lt(1),
+    "Cycle Paths Solar": ee.FeatureCollection('projects/data-sunlight-311713/assets/cyclenet').reduceToImage(properties= ['FID'], reducer= ee.Reducer.first()).unmask().lt(1),
     "Railway": ee.FeatureCollection('projects/data-sunlight-311713/assets/traintracks').reduceToImage(properties = ['FEATCODE'], reducer = ee.Reducer.first()).unmask().lt(1),
     "Areas of Natural Beauty": ee.FeatureCollection("projects/data-sunlight-311713/assets/Areas_of_Outstanding_Natural_Beauty_England").reduceToImage(properties= ['stat_area'], reducer= ee.Reducer.first()).unmask().lt(1),
     "Protected Areas": ee.FeatureCollection("projects/data-sunlight-311713/assets/gb_protected_areas_nobuffer").reduceToImage(properties= ['Shape_Area'], reducer= ee.Reducer.first()).unmask().gt(0).eq(0),
     "Surface Water":ee.FeatureCollection('projects/data-sunlight-311713/assets/UK_SurfaceWater_Area_Buffer_50m').reduceToImage(properties= ['FEATCODE'], reducer= ee.Reducer.first()).unmask().lt(1),
-    "Cultural Sites": ee.FeatureCollection('projects/data-sunlight-311713/assets/england_culturalsites').reduceToImage(properties = ['ListEntry'], reducer = ee.Reducer.first()).unmask().lt(1),
-    "Parks and Green Space": ee.FeatureCollection("projects/data-sunlight-311713/assets/GreenspaceEngArea").reduceToImage(properties= ['areaHa'], reducer= ee.Reducer.first()).unmask().lt(1),
+    "England Cultural Sites": ee.FeatureCollection('projects/data-sunlight-311713/assets/england_culturalsites').reduceToImage(properties = ['ListEntry'], reducer = ee.Reducer.first()).unmask().lt(1),
+    "Parks and Green Space": ee.FeatureCollection("projects/data-sunlight-311713/assets/GB_GreenspaceSite").reduceToImage(properties= ['areaHa'], reducer= ee.Reducer.first()).unmask().lt(1),
     "Functional Sites": ee.FeatureCollection('projects/data-sunlight-311713/assets/Functional_sites').reduceToImage(properties= ['FEATCODE'], reducer= ee.Reducer.first()).unmask().lt(1),
-    "Built Up Areas": get_build_up_area_buffer(100).unmask().lt(1),
+    "Built Up Areas": get_build_up_area_buffer(250).unmask().lt(1),
     "Existing Solar PV": ee.FeatureCollection('projects/data-sunlight-311713/assets/solar_pv').reduceToImage(properties= ['FID'], reducer = ee.Reducer.first()).unmask().lt(1),
     "Existing Other Renewable":ee.FeatureCollection('projects/data-sunlight-311713/assets/other_renewables').reduceToImage(properties= ['FID'], reducer= ee.Reducer.first()).unmask().lt(1),
     "Existing Onshore Wind": ee.FeatureCollection('projects/data-sunlight-311713/assets/onshore_wind').reduceToImage(properties= ['FID'], reducer= ee.Reducer.first()).unmask().lt(1),
     "Flood Risk Zone 2": ee.FeatureCollection('users/Josh_Redmond/EA_FloodMapForPlanningRiversAndSeaFloodZone2_SHP_Full').reduceToImage(properties= ['st_area_sh'], reducer= ee.Reducer.first()).unmask().lt(1),
     "Flood Risk Zone 3": ee.FeatureCollection('users/Josh_Redmond/EA_FloodMapForPlanningRiversAndSeaFloodZone3_SHP_Full').reduceToImage(properties= ['st_area_sh'], reducer= ee.Reducer.first()).unmask().lt(1),
     #"High Grade Agricultural":
+    'Scotland Cultural Sites': ee.FeatureCollection('projects/data-sunlight-311713/assets/ScotlandCulturalSites').map(lambda x: x.buffer(250)).map(lambda x: x.set('dummy', 1)).reduceToImage(properties= ['dummy'], reducer= ee.Reducer.first()).unmask().lt(1),
+    'Scotland Agricultural Land': ee.FeatureCollection('projects/data-sunlight-311713/assets/ScotlandAgriculturalLand').reduceToImage(properties= ['LCCODE '], reducer= ee.Reducer.first()).unmask().lte(2),
     "England National Parks": ee.FeatureCollection('projects/data-sunlight-311713/assets/EnglandNationalParks').reduceToImage(properties= ['code'], reducer= ee.Reducer.first()).unmask().lt(1),
     "Wales National Park": ee.FeatureCollection('projects/data-sunlight-311713/assets/WalesNatPark').reduceToImage(properties= ['ISIS_ID'], reducer= ee.Reducer.first()).unmask().lt(1),
     "Scotland Scenic Areas": ee.FeatureCollection('projects/data-sunlight-311713/assets/ScotlandNSAs').reduceToImage(properties= ['dummy'], reducer= ee.Reducer.first()).unmask().lt(1),
-    "Agricultural Land": ee.FeatureCollection('projects/data-sunlight-311713/assets/AgriculturalLandEngland').reduceToImage(properties= ['hectares'], reducer= ee.Reducer.first()).unmask().lt(1)
+    "England Agricultural Land": ee.FeatureCollection('projects/data-sunlight-311713/assets/AgriculturalLandEngland').reduceToImage(properties= ['hectares'], reducer= ee.Reducer.first()).unmask().lt(1)
     }
 
     return exclusions_dict
@@ -120,9 +123,10 @@ def getExclusions(type="common"):
     "Protected Areas",
     "Areas of Natural Beauty",
     "Woodlands",
-    "Cycle Paths",
+    "Cycle Paths Wind",
     "Surface Water",
-    "Cultural Sites",
+    "England Cultural Sites",
+    "Scotland Cultural Sites",
     "Parks and Green Space",
     "Functional Sites",
     "England National Parks",
@@ -132,7 +136,9 @@ def getExclusions(type="common"):
 
     solar_exclusions = ["Slope > 10",
     "Existing Solar PV",
-    "Agricultural Land"]
+    "Cycle Paths Solar",
+    "Scotland Agricultural Land",
+    "England Agricultural Land"]
 
     listDict = {"solar":solar_exclusions, "common":common_exclusions, "wind":wind_exclusions}
 
